a015052bf930ec77fd4e28b9d306fb03c1872adb
CODONIC <xtianbert@gmail.com>
2024-04-08 10:49:13 +0800

    Fixed dash button

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -23,7 +23,46 @@ public class PlayerMovement : MonoBehaviour
         UpdateAnimation(joystickInput);
     }
 
-    void MoveCharacter(Vector3 input)
+    public Vector3 GetFacingDirection()
+    {
+        // Get the rotation of the player's sprite
+        Quaternion rotation = transform.rotation;
+
+        // Calculate the facing direction based on the sprite's rotation
+        Vector3 facingDirection = Vector3.up; // Default facing direction is up (0, 1, 0)
+
+        // Determine the angle between the forward direction and each cardinal direction
+        float angleUp = Vector3.Angle(transform.up, Vector3.up);
+        float angleRight = Vector3.Angle(transform.right, Vector3.up);
+        float angleDown = Vector3.Angle(-transform.up, Vector3.up);
+        float angleLeft = Vector3.Angle(-transform.right, Vector3.up);
+
+        // Find the direction closest to the current rotation
+        float minAngle = Mathf.Min(angleUp, angleRight, angleDown, angleLeft);
+        if (minAngle == angleUp)
+        {
+            // Facing up
+            facingDirection = Vector3.up;
+        }
+        else if (minAngle == angleRight)
+        {
+            // Facing right
+            facingDirection = Vector3.right;
+        }
+        else if (minAngle == angleDown)
+        {
+            // Facing down
+            facingDirection = Vector3.down;
+        }
+        else if (minAngle == angleLeft)
+        {
+            // Facing left
+            facingDirection = Vector3.left;
+        }
+        return facingDirection;
+    }
+
+    public void MoveCharacter(Vector3 input)
     {
         Vector3 movement = input.normalized * moveSpeed * Time.deltaTime;
         transform.position += movement;
